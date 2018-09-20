189b3133b830b93a3a5307212e3a74523f2c8bc7
Kevin Glavin <kglavin@gmail.com>
2018-09-20 16:24:32 -0700

    correcting addressing for test environment

--- scm-snmp/poller-snmp.py
+++ scm-snmp/poller-snmp.py
@@ -62,11 +62,22 @@ if __name__ == "__main__":
     netrc = netrc.netrc()
 
     sites = { 
+             'Memphis':'44.1.0.10',
+             'Kansas':'44.1.0.11',
              'Dallas':'44.1.0.12',
              'Denver':'44.1.0.13',
              'Albuquerque':'44.1.0.14',
              'ElPaso':'44.1.0.15',
-             'Houston':'44.1.0.16'
+             'Houston':'44.1.0.16',
+             'NewOrleans':'44.1.0.17',
+             'SanAntonio':'44.1.0.18',
+             'Amsterdam':'44.1.0.21',
+             'Frankfurt':'44.1.0.23',
+             'Paris':'44.1.0.22',
+             'Bangkok':'44.1.0.31',
+             'HochiMihn':'44.1.0.33',
+             'KualaLumpar':'44.1.0.32',
+
             }
 
     site_data = { }
