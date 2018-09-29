9543638d35884ff59651611c1ac65869db6e2fce
kglavin <kglavin@gmail.com>
2018-09-28 19:30:12 -0700

    cleanup of the page generation code

--- scm-mapapp/multi_page.py
+++ scm-mapapp/multi_page.py
@@ -16,6 +16,40 @@ app = dash.Dash(__name__)
 # needed to keep dash happy about complex dependancies with multi-page app.
 app.config.supress_callback_exceptions = True
 
+def traffic_dropdowns(sitedf):
+    sites = sitedf['site'].tolist()
+    interfaces = [ 0,1,2,3,4]
+    tun=['eth','tun']
+    duration = [ '1h', '30m', '5m', '24h', '7d']
+    gtype=['Packets','Octets']
+
+    d1 = dcc.Dropdown(id='if-stats-site',
+                      options=[{'label': i, 'value': i} for i in sites],
+                      value='Albuquerque',
+                      className='three columns')
+    d2 = dcc.Dropdown(id='if-stats-tun',
+                      options=[{'label': i, 'value': i } for i in tun ],
+                      value='eth',
+                      className='one column')
+    d3 = dcc.Dropdown(id='if-stats-eth',
+                      options=[{'label': "if-"+str(i), 'value': i } for i in interfaces ],
+                      value='0',
+                      className='three columns')
+    d4 = dcc.Dropdown(id='if-stats-duration',
+                      options=[{'label': "Past Duration: "+i, 'value': i } for i in duration ],
+                      value='1h',
+                      className='three columns')
+    d5 = dcc.Dropdown(id='if-stats-packets',
+                      options=[{'label': "Type: "+i, 'value': i } for i in gtype ],
+                      value='Octets',
+                      className='three columns')
+    return [ d1,d2,d3,d4,d5]
+
+def traffic_html(sitedf):
+    return html.Div(children = [ html.Div( children = traffic_dropdowns(sitedf),className='row'),
+                                dcc.Graph(id='if-stats-graph')],
+                    className="twelve columns")
+
 def generate_table(dataframe, max_rows=10):
     return html.Table(
         # Header
@@ -27,101 +61,65 @@ def generate_table(dataframe, max_rows=10):
         ]) for i in range(min(len(dataframe), max_rows))]
     )
 
-def traffic_html(sitedf):
-    sites = sitedf['site'].tolist()
-    interfaces = [ 0,1,2,3,4]
-    tun=['eth','tun']
-    duration = [ '1h', '30m', '5m', '24h', '7d']
-    gtype=['Packets','Octets']
-    return html.Div( children = [  
-                html.Div( children = [ 
-                    dcc.Dropdown(
-                      id='if-stats-site',
-                      options=[{'label': i, 'value': i} for i in sites],
-                      value='Albuquerque',
-                      className='three columns'
-                    ),
-                    dcc.Dropdown(
-                      id='if-stats-tun',
-                      options=[{'label': i, 'value': i } for i in tun ],
-                      value='eth',
-                      className='one column'
-                    ),
-                    dcc.Dropdown(
-                      id='if-stats-eth',
-                      options=[{'label': "if-"+str(i), 'value': i } for i in interfaces ],
-                      value='0',
-                      className='three columns'
-                    ),
-                    dcc.Dropdown(
-                      id='if-stats-duration',
-                      options=[{'label': "Past Duration: "+i, 'value': i } for i in duration ],
-                      value='1h',
-                      className='three columns'
-                    ),
-                    dcc.Dropdown(
-                      id='if-stats-packets',
-                      options=[{'label': "Type: "+i, 'value': i } for i in gtype ],
-                      value='Octets',
-                      className='three columns'
-                    )],
-                    className='row'
-                    ),
-                    dcc.Graph(id='if-stats-graph')
-                ],
-                className="twelve columns"
-           )
-
 def event_html():
-    return html.Div([
-                html.H4(children='Event Log'),
+    return html.Div(children=[
+                html.H4('Event Log'),
                 generate_table(eventdf,500),]
             )
 
-def map_html():
+
+def map_dropdowns(regions):
     opts = [ {'label':'All Sites', 'value': 0} ]
-    opts.extend([{'label': 'Region '+str(i), 'value': i} for i in globals()['regions']])
-    return html.Div( children = [  
-                html.Div(
-                    dcc.Dropdown(
-                        id='map-refresh',
+    opts.extend([{'label': 'Region '+str(i), 'value': i} for i in regions])
+
+    d1 = dcc.Dropdown(id='map-refresh',
                         options=opts,
-                        value=0
-                    ),
-                ),
+                        value=0)
+    return [ d1 ]
+
+
+def map_html(regions):
+    return html.Div( children = [  
+                html.Div( children = map_dropdowns(regions), classname='row'),
                 dcc.Graph(id='sites-map')
                 ],
                 className="twelve columns"
            )
 
-app.layout = html.Div([
-    heading_html(),
-    dcc.Location(id='url', refresh=False),
-    html.Div([
-        dcc.Link(html.Button('home',    id='home_page'),    href='/'),
-        dcc.Link(html.Button('map',     id='map_page'),     href='/map_page'),
-        dcc.Link(html.Button('traffic', id='traffic_page'), href='/traffic_page'),
-        dcc.Link(html.Button('event',   id='event_page'),   href='/event_page'),
-        ]),
-    html.Br(),
-    html.Div(id='page-content'),
-    footer_html()
+def scm_buttons():
+    b1 = dcc.Link(html.Button('home',    id='home_page'),    href='/')
+    b2 = dcc.Link(html.Button('map',     id='map_page'),     href='/map_page')
+    b3 = dcc.Link(html.Button('traffic', id='traffic_page'), href='/traffic_page')
+    b4 = dcc.Link(html.Button('event',   id='event_page'),   href='/event_page')
+    return [ b1, b2, b3, b4]
 
-])
+def scm_layout():
+
+    r =   html.Div(children =[
+                    heading_html(),
+                    dcc.Location(id='url', refresh=False),
+                    html.Div(children= scm_buttons()),
+                    html.Br(),
+                    html.Div(id='page-content'),
+                    footer_html()
+                    ])
+    return r
+
+app.layout = scm_layout()
 
 @app.callback(dash.dependencies.Output('sites-map', 'figure'),
               [dash.dependencies.Input('map-refresh', 'value')])
-def gen_map(value):
-    ##TODO
+def gen_map(region):
     ## for each map update hit the local proxy to get the most recently polled sitesdf
     # this may still be stale data on the proxy but its responsive data. 
     sitesdf = get_sites_proxy(globals()['proxy'])
-    tun_list = generate_tunnels(sitedf,region=value)
-    tun_list.append(generate_sites(sitedf, region=value))
-    #TODO: based on the generated site list we should change the center of focus 
-    #TODO: can we focus the zoom of the map to just contain the points in the set?
+    tun_list = generate_tunnels(sitedf,region)
+    tun_list.append(generate_sites(sitedf, region))
+    #based on the generated site list we should change the center of focus 
     (mid_lat, mid_lon) = latlon_midpoint(sitedf,region=value)
-    # for global networks, this calculation althught mathamatically correct is not pleasing so limit center to no be about 50 degrees of latitde
+    
+    #TODO: can we focus the zoom of the map to just contain the points in the set?
+    # for global networks, this calculation although mathamatically correct is not pleasing so limit center to no be about 50 degrees of latitde
     if (mid_lat > 50):
         mid_lat = 50
     if (mid_lat < -50):
@@ -137,7 +135,9 @@ def gen_map(value):
     #if region > 0:
     #    mbox['zoom'] = 3
 
-
+    
+    # This is the midpoint drawn as a triangle on the map -- 
+    # TODO, will use this ( or another icon as the place to show a full mesh terminating)
     m_point = {
             'lat': [mid_lat],
             'lon': [mid_lon],
@@ -147,6 +147,7 @@ def gen_map(value):
             'text': "Center"
         }
     tun_list.append(m_point)
+
     figure={       
             'data': tun_list,
             'layout': {   
@@ -162,6 +163,19 @@ def gen_map(value):
     return figure
 
 
+def gen_if_stats_data(site,tun,eth,duration,packets):
+    qd = {'id':site, 'if_name':str(tun)+str(eth), 'period':duration}
+    data = query_scmdata("ifstats", query_data=qd)
+
+    if data.size > 0:
+        if packets in 'Octets':
+            data['in'] = pd.to_numeric(data['in_octets'], errors='coerce').diff()
+            data['out'] = pd.to_numeric(data['out_octets'], errors='coerce').diff()
+        else:
+            data['in'] = pd.to_numeric(data['in_unicast'], errors='coerce').diff()
+            data['out'] = pd.to_numeric(data['out_unicast'], errors='coerce').diff()
+    return data
+
 @app.callback(output=dash.dependencies.Output('if-stats-graph', 'figure'),
               inputs=[dash.dependencies.Input('if-stats-site', 'value'),
               dash.dependencies.Input('if-stats-tun', 'value'),
@@ -170,18 +184,9 @@ def gen_map(value):
               dash.dependencies.Input('if-stats-packets', 'value')
               ])
 def gen_if_stats_graphs(site,tun,eth,duration,packets):
-    figure = {}
-    qd = {'id':site, 'if_name':str(tun)+str(eth), 'period':duration}
-    data = query_scmdata("ifstats", query_data=qd)
-    derived_data = pd.DataFrame()
-    if data.size > 0:
-        if packets in 'Octets':
-            data['in'] = pd.to_numeric(data['in_octets'], errors='coerce').diff()
-            data['out'] = pd.to_numeric(data['out_octets'], errors='coerce').diff()
-        else:
-            data['in'] = pd.to_numeric(data['in_unicast'], errors='coerce').diff()
-            data['out'] = pd.to_numeric(data['out_unicast'], errors='coerce').diff()
+        data=gen_if_stats_data(site,tun,eth,duration,packets)
 
+        # two lines on the graph ( and in and an out)
         figure={
             'data': [{
                     'x': data.index,
@@ -210,7 +215,7 @@ def display_page(pathname):
         r = html.Div("/home_page")
     else:
         if pathname == '/map_page':
-            r = map_html()
+            r = map_html(regions)
         if pathname == '/traffic_page':
             r = traffic_html(sitedf)
         if pathname == '/event_page':
