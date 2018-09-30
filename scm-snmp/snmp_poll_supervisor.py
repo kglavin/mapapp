32a1a4154123366d87244e19b78ba44c5e39227c
kglavin <kglavin@gmail.com>
2018-09-29 18:09:12 -0700

    adding proxy get of sides

--- scm-snmp/snmp_poll_supervisor.py
+++ scm-snmp/snmp_poll_supervisor.py
@@ -18,16 +18,33 @@
 # so batch insert should be fast across a high speed low latency connection. 
 
 from easysnmp import Session
+import pandas as pd
+import requests as rq
 import multiprocessing as mp
 import netrc 
 import time
 from poller_snmp import mp_poll_sites, poll_sites, pivot_sitedata
 from influxdb import InfluxDBClient
-import pprint
+import logging
+
+def update_sites_dict(proxy='http://127.0.0.1:8040'):
+    ret = {}
+    r = rq.get( proxy + '/api/snmp_details', auth=(user,pw))
+    if r.status_code == 200:
+        df =  pd.read_json(r.content, orient='index')
+        ret = dict(zip(df.site, df.v4ip))
+    return ret
 
 if __name__ == "__main__":
     netrc = netrc.netrc()
+    logger = logging.getLogger('scm-snmp-poll')
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('scm-snmp-poll.log')
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger.addHandler(fh)
 
+    # initial site list, will be overwritten by dynamic list one its been calulated from SCM instances.
     sites = { 
              'Memphis':'44.1.0.10',
              'Kansas':'44.1.0.11',
@@ -53,23 +70,65 @@ if __name__ == "__main__":
     client.switch_database('scmdata')
     pool = mp.Pool(processes=4)
 
+    measurement_count = 0
+    good_measurement_writes = 0
+    bad_measurement_writes = 0
+    poll_time_measures = []
+
+    sites_refresh_count = 0
+    old_sites = {}
+
+    logging.debug('starting main loop')
     while True:
-        measurements = [ ]
+        sites_refresh_count += 1
+        if sites_refresh_count > 20: 
+            sites_refresh_count = 0
+            logging.info(f'''measurements: good writes - {good_measurement_writes}, bad writes - {bad_measurement_writes}, total metrics: {measurement_count}''')
+            measurement_count = 0
+            good_measurement_writes = 0
+            bad_measurement_writes = 0
+            poll_time_measures = []
+            new_sites = update_sites_dict()
+            if len(new_sites > 0):
+                old_sites = sites
+                sites = new_sites
+            else:
+                #continue with existing sites 
+                #but if sites is also empty try old sites
+                logging.debug('new_sites return empty')
+                if len(sites) < 1:
+                    sites = old_sites
+                    logging.debug('reverting to using old_sites')
+
         start_poll_time = time.time()
-        site_data = mp_poll_sites(pool,sites,community)
-        for k,v in site_data.items():
-          for n in pivot_sitedata(v,'eth'):
-             measurements.append(n)
-          for n in pivot_sitedata(v,'vti'):
-             measurements.append(n)
-        try:
-          client.write_points(measurements,time_precision='s')
-        except:
-            pass
+        if len(sites) > 0:            
+            measurements = [ ]
+            site_data = mp_poll_sites(pool,sites,community)
+            for k,v in site_data.items():
+              for n in pivot_sitedata(v,'eth'):
+                 measurements.append(n)
+                 measurement_count +=1
+              for n in pivot_sitedata(v,'vti'):
+                 measurements.append(n)
+                 measurement_count +=1
+            try:
+              client.write_points(measurements,time_precision='s')
+              good_measurement_writes +=1
+            except:
+                bad_measurement_writes +=1
+                logging.debug('failed to write_points')
+                pass
         now_time = time.time()
         poll_time = now_time-start_poll_time
         if int(poll_time) < 15:
-          time.sleep(15-int(poll_time))
+            poll_time_measures.append(poll_time)
+            if sites_refresh_count == 20:
+                logging.info('poll_times:'.join(str(e) for e in poll_time_measures))
+                poll_time_measures = []
+            time.sleep(15-int(poll_time))
         else:
-          time.sleep(5)
+            logging.debug('poll took longer that 15, throttling by 5')
+            logging.info('poll_times:'.join(str(e) for e in poll_time_measures))
+            poll_time_measures = []
+            time.sleep(5)
 
