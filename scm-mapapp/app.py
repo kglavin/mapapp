265b2c4b0dbfd8111ee34f964e47ba8f24c40aad
kglavin <kglavin@gmail.com>
2018-09-30 11:45:58 -0700

    calculate time diff and use to populate with rate measurement on graph

--- scm-mapapp/app.py
+++ scm-mapapp/app.py
@@ -197,12 +197,13 @@ def gen_if_stats_data(site,tun,eth,duration,packets):
     data = query_scmdata("ifstats", query_data=qd)
 
     if data.size > 0:
+        df['deltaT'] = data.index.to_series().diff().dt.seconds.div(60, fill_value=30)
         if packets in 'Octets':
-            data['in'] = pd.to_numeric(data['in_octets'], errors='coerce').diff()
-            data['out'] = pd.to_numeric(data['out_octets'], errors='coerce').diff()
+            data['in'] = pd.to_numeric(data['in_octets'], errors='coerce').diff()/df['deltaT']
+            data['out'] = pd.to_numeric(data['out_octets'], errors='coerce').diff()/df['deltaT']
         else:
-            data['in'] = pd.to_numeric(data['in_unicast'], errors='coerce').diff()
-            data['out'] = pd.to_numeric(data['out_unicast'], errors='coerce').diff()
+            data['in'] = pd.to_numeric(data['in_unicast'], errors='coerce').diff()/df['deltaT']
+            data['out'] = pd.to_numeric(data['out_unicast'], errors='coerce').diff()/df['deltaT']
     return data
 
 @app.callback(output=dash.dependencies.Output('if-stats-graph', 'figure'),
