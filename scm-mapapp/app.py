d9dfe04c0aea7585e518f137e58c8b02584683ae
kglavin <kglavin@gmail.com>
2018-10-03 18:50:13 -0700

    c

--- scm-mapapp/app.py
+++ scm-mapapp/app.py
@@ -168,10 +168,8 @@ def gen_map(region):
 
     tun_list = generate_tunnels(df,region)
     rl, gl = generate_sites(green_df, df, region)
-    if len(gl) > 0:
-        tun_list.append(gl)
-    if len(rl) > 0:
-        tun_list.append(rl)
+    tun_list.append(rl)
+    tun_list.append(gl)
     
     #TODO: can we focus the zoom of the map to just contain the points in the set?
     # for global networks, this calculation although mathamatically correct is not pleasing so limit center to no be about 50 degrees of latitde
