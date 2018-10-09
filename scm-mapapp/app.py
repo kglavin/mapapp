370a62cb515f989dca50657811fd67764a326019
kglavin <kglavin@gmail.com>
2018-10-09 12:22:20 -0700

    c

--- scm-mapapp/app.py
+++ scm-mapapp/app.py
@@ -29,21 +29,21 @@ def home_regions():
     r = html.Div( children = [
             html.H3('Managing [3] Regions'),
             html.P('Region 1'), 
-            html.A(html.Button('Launch Region1 SCM', className='two columns'),
+            html.A(html.Button('Launch Region1 SCM'),
                                 href='https://catfish3.riverbed.cc/admin/kglavin-us'
                     ),
-            html.Br(className=row),
+            html.Br(),
             html.P('Region 2'),
 
             html.A(html.Button('Launch Region2 SCM', className='two columns'),
                                 href='https://catfish3.riverbed.cc/admin/kglavin-eur'
                     ),
-            html.Br(className=row),
+            html.Br(),
             html.P('Region 3'), 
             html.A(html.Button('Launch Region3 SCM', className='two columns'),
                                 href='https://catfish3.riverbed.cc/admin/kglavin-asia'
                     ),
-            html.Br(className=row),
+            html.Br(),
         ]
         )
     return r
